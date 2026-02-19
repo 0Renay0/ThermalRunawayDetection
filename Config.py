5b4a4ccd3c1231882758fb3c9e62a7ef3a189a48
Rayen <rayenhamadi48@gmail.com>
2026-02-19 14:43:00 +0100

    pre-commit

--- Config.py
+++ Config.py
@@ -1,42 +1,41 @@
-import numpy as np 
-
+import numpy as np
 
 
 # ======== Cinétique ========
-k01_100 = 5.25e-5   # L/mol/s
+k01_100 = 5.25e-5  # L/mol/s
 k02_100 = 7e-8
 k03_100 = 0.3e-8
 
-Ea1 = 57000.0       # J/mol
+Ea1 = 57000.0  # J/mol
 Ea2 = 220000.0
 Ea3 = 250000.0
 
-H1 = -10000.0       # J/mol
+H1 = -10000.0  # J/mol
 H2 = -95000.0
 H3 = -5000.0
 
 # ======== CI & paramètres physiques ========
-Rg = 8.314          # J/mol/K
-UA = 10.0           # W/K
-Cpr = 2500.0        # J/kg/K
-mr0 = 90.0          # kg
-rho = 1.0           # kg/L (tel que utilisé dans ton code)
+Rg = 8.314  # J/mol/K
+UA = 10.0  # W/K
+Cpr = 2500.0  # J/kg/K
+mr0 = 90.0  # kg
+rho = 1.0  # kg/L (tel que utilisé dans ton code)
 
 Tconv = 273.15
 Tr0 = 65.0 + Tconv  # K
-Tj = Tr0            # K
+Tj = Tr0  # K
 
-kla = 0.23          # s^-1 (O2 liquide)
-kla2 = 3.5          # s^-1 (gaz pour espèces ≠ O2)
+kla = 0.23  # s^-1 (O2 liquide)
+kla2 = 3.5  # s^-1 (gaz pour espèces ≠ O2)
 
-Vheadspace = 50.0 / 1000.0   # m^3
-PN2 = 10.0 * 100000.0        # Pa
+Vheadspace = 50.0 / 1000.0  # m^3
+PN2 = 10.0 * 100000.0  # Pa
 
 # ======== Enthalpies de vaporisation (J/mol) ========
 Hvap_CA = 28943.0
-Hvap_W  = 42876.0
+Hvap_W = 42876.0
 Hvap_Epi = 41775.0
-Hvap_HP  = 41638.0
+Hvap_HP = 41638.0
 Hvap_diol = 66149.0
 
 # ======== Masses molaires (kg/mol) ========
@@ -44,16 +43,20 @@ M_o2 = 16e-3
 M_CA = 76.53e-3
 M_HP = 34e-3
 M_Ep = 92.53e-3
-M_W  = 18e-3
+M_W = 18e-3
+
 
 # ======== Conditions initiales ========
 def initial_state():
     # [CA, HP, Ep, RO, W, O2_liq, nO2_gas, nCA_gas, nHP_gas, nEp_gas, nW_gas, mr, Tr, P]
-    return np.array([7.26, 9.15, 0.0, 0.0, 7.41, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mr0, Tr0, PN2], dtype=float)
+    return np.array(
+        [7.26, 9.15, 0.0, 0.0, 7.41, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mr0, Tr0, PN2],
+        dtype=float,
+    )
+
 
 # ---------- Grille de simulation ----------
 def time_grid():
     t_span = (0.0, 50000.0)
     t_eval = np.linspace(t_span[0], t_span[1], 501)
     return t_span, t_eval
-
--- ode_model.py
+++ ode_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import Config as cfg
 
+
 def rhs(t, c):
     CA, HP, Ep, RO, W, O2_liq, n02_gas, nCA_gas, nHP_gas, nEp_gas, nW_gas, mr, Tr, P = c
 
@@ -11,57 +12,76 @@ def rhs(t, c):
 
     # Vitesses
     R1 = k1 * CA * HP
-    R2 = k2 * (HP ** 2)
+    R2 = k2 * (HP**2)
     R3 = k3 * Ep * W
 
     # Solubilité O2 (mol/L)
     O2_star = 0.001473 * np.exp(-0.008492 * (Tr - cfg.Tconv))
 
-    # Équilibres gaz (mol/m^3) 
-    W_star_g  = (10 ** (11.008 - (2239.7 / Tr))) / (cfg.Rg * Tr)
+    # Équilibres gaz (mol/m^3)
+    W_star_g = (10 ** (11.008 - (2239.7 / Tr))) / (cfg.Rg * Tr)
     HP_star_g = (10 ** (9.9669 - (2175.0 / Tr))) / (cfg.Rg * Tr)
     CA_star_g = (10 ** (9.7621 - (1511.9 / Tr))) / (cfg.Rg * Tr)
     Ep_star_g = (10 ** (10.671 - (2182.2 / Tr))) / (cfg.Rg * Tr)
-    # Diol_star_g = (10 ** (12.266 - (3455.4 / Tr))) / (cfg.Rg * Tr)  # Negligé 
+    # Diol_star_g = (10 ** (12.266 - (3455.4 / Tr))) / (cfg.Rg * Tr)  # Negligé
 
-    # O2 transfert 
+    # O2 transfert
     kla_effective = cfg.kla if (O2_liq - O2_star) >= 0 else 0.0
 
     # O2 liquide / gaz
     dO2_liqdt = R2 - kla_effective * (O2_liq - O2_star)
     dn02_gasdt = kla_effective * (mr / cfg.rho) * (O2_liq - O2_star)
 
-    # Variation de masse (kg/s) 
-    dmrdt = - (dn02_gasdt * cfg.M_o2) \
-            - cfg.Vheadspace * (
-                (cfg.kla2 * (CA_star_g - nCA_gas / cfg.Vheadspace)) * cfg.M_CA
-                + (cfg.kla2 * (HP_star_g - nHP_gas / cfg.Vheadspace)) * cfg.M_HP
-                + (cfg.kla2 * (Ep_star_g - nEp_gas / cfg.Vheadspace)) * cfg.M_Ep
-                + (cfg.kla2 * (W_star_g  - nW_gas  / cfg.Vheadspace)) * cfg.M_W
-            )
+    # Variation de masse (kg/s)
+    dmrdt = -(dn02_gasdt * cfg.M_o2) - cfg.Vheadspace * (
+        (cfg.kla2 * (CA_star_g - nCA_gas / cfg.Vheadspace)) * cfg.M_CA
+        + (cfg.kla2 * (HP_star_g - nHP_gas / cfg.Vheadspace)) * cfg.M_HP
+        + (cfg.kla2 * (Ep_star_g - nEp_gas / cfg.Vheadspace)) * cfg.M_Ep
+        + (cfg.kla2 * (W_star_g - nW_gas / cfg.Vheadspace)) * cfg.M_W
+    )
 
-    # Phase liquide (mol/L/s) 
-    vol_liq_L = (mr / cfg.rho)  
-    gas2liq_factor = (cfg.Vheadspace / vol_liq_L)
+    # Phase liquide (mol/L/s)
+    vol_liq_L = mr / cfg.rho
+    gas2liq_factor = cfg.Vheadspace / vol_liq_L
 
-    dCAdt = -R1 - (cfg.kla2 * (CA_star_g - nCA_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor - (CA / mr) * dmrdt
-    dHPdt = -R1 - 2.0 * R2 - (cfg.kla2 * (HP_star_g - nHP_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor - (HP / mr) * dmrdt
-    dEpdt =  R1 - R3 - (cfg.kla2 * (Ep_star_g - nEp_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor - (Ep / mr) * dmrdt
-    dROdt =  R3 - (RO / mr) * dmrdt
-    dWdt  =  R1 + 2.0 * R2 - R3 - (cfg.kla2 * (W_star_g - nW_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor - (W / mr) * dmrdt
+    dCAdt = (
+        -R1
+        - (cfg.kla2 * (CA_star_g - nCA_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor
+        - (CA / mr) * dmrdt
+    )
+    dHPdt = (
+        -R1
+        - 2.0 * R2
+        - (cfg.kla2 * (HP_star_g - nHP_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor
+        - (HP / mr) * dmrdt
+    )
+    dEpdt = (
+        R1
+        - R3
+        - (cfg.kla2 * (Ep_star_g - nEp_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor
+        - (Ep / mr) * dmrdt
+    )
+    dROdt = R3 - (RO / mr) * dmrdt
+    dWdt = (
+        R1
+        + 2.0 * R2
+        - R3
+        - (cfg.kla2 * (W_star_g - nW_gas / cfg.Vheadspace) / 1000.0) * gas2liq_factor
+        - (W / mr) * dmrdt
+    )
 
-    # Phase gazeuse (mol/s) 
+    # Phase gazeuse (mol/s)
     dnCA_gasdt = cfg.Vheadspace * cfg.kla2 * (CA_star_g - nCA_gas / cfg.Vheadspace)
     dnHP_gasdt = cfg.Vheadspace * cfg.kla2 * (HP_star_g - nHP_gas / cfg.Vheadspace)
     dnEp_gasdt = cfg.Vheadspace * cfg.kla2 * (Ep_star_g - nEp_gas / cfg.Vheadspace)
-    dnW_gasdt  = cfg.Vheadspace * cfg.kla2 * (W_star_g  - nW_gas  / cfg.Vheadspace)
+    dnW_gasdt = cfg.Vheadspace * cfg.kla2 * (W_star_g - nW_gas / cfg.Vheadspace)
 
-    # Evaporation 
+    # Evaporation
     qevapo = (
         dnCA_gasdt * cfg.Hvap_CA
         + dnHP_gasdt * cfg.Hvap_HP
         + dnEp_gasdt * cfg.Hvap_Epi
-        + dnW_gasdt  * cfg.Hvap_W
+        + dnW_gasdt * cfg.Hvap_W
     )
 
     # Bilan thermique
@@ -80,6 +100,19 @@ def rhs(t, c):
     dn_tot_dt = dn02_gasdt + dnCA_gasdt + dnHP_gasdt + dnEp_gasdt + dnW_gasdt
     dPdt = (cfg.Rg / cfg.Vheadspace) * (dn_tot_dt * Tr + n_tot * dTrdt)
 
-    return [dCAdt, dHPdt, dEpdt, dROdt, dWdt,
-            dO2_liqdt, dn02_gasdt, dnCA_gasdt, dnHP_gasdt, dnEp_gasdt, dnW_gasdt,
-            dmrdt, dTrdt, dPdt]
+    return [
+        dCAdt,
+        dHPdt,
+        dEpdt,
+        dROdt,
+        dWdt,
+        dO2_liqdt,
+        dn02_gasdt,
+        dnCA_gasdt,
+        dnHP_gasdt,
+        dnEp_gasdt,
+        dnW_gasdt,
+        dmrdt,
+        dTrdt,
+        dPdt,
+    ]
