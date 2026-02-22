6ffb6ff436fda8ef07eb157dc021273cfccc5f74
Rayen <rayenhamadi48@gmail.com>
2026-02-22 22:38:41 +0100

    Inverse logique detection

--- AnomalyDetection.py
+++ AnomalyDetection.py
@@ -137,9 +137,9 @@ def detect_anomalies(
 
     flag = (anomaly_score > thr).astype(int)
     flag[mask_base] = 0
-    gate_T = df.loc[X.index, "d2Tdt2"].to_numpy(dtype=float) > 0.0
-    gate_P = df.loc[X.index, "d2Pdt2"].to_numpy(dtype=float) > 0.0
-    gate = gate_T | gate_P
+    gate_T2 = df.loc[X.index, "d2Tdt2"].to_numpy(dtype=float) > 0.0
+    gate_T = df.loc[X.index, "dTdt"].to_numpy(dtype=float) > 0.0
+    gate = gate_T & gate_T2
     flag = flag * gate.astype(int)
 
     flag_persist = (
