38dc18d2a5db204d5b0077f9afda90b79877b4e0
Rayen <rayenhamadi48@gmail.com>
2026-02-24 09:56:50 +0100

    fit nominal model

--- AnomalyDetectionFS.py
+++ AnomalyDetectionFS.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
 from AnomalyDetection import compute_RCI, make_windows_features
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import RobustScaler
 
 
 def build_features(
@@ -45,3 +48,52 @@ def build_features(
     X["TP_corr"] = tp_corr.loc[X.index]
 
     return df, X
+
+
+def fit_nominal_model(
+    nominal_datasets: list[pd.DataFrame],
+    time_col="Time",
+    T_col="Tr_K",
+    P_col="Pression_ideal_bar",
+    win=15,
+    baseline_end_s=None,
+    contamination=0.1,
+    random_state=0,
+    thr_quantile=0.95,
+):
+    """
+    Fit the nominal model (IsolationForest) on the nominal datasets
+    """
+
+    X_list = []
+    for data in nominal_datasets:
+        df, X = build_features(
+            data, time_col=time_col, T_col=T_col, P_col=P_col, win=win
+        )
+        if baseline_end_s is None:
+            X_list.append(X)
+        else:
+            mask = (df.loc[X.index, time_col] <= baseline_end_s).to_numpy()
+            X_list.append(X.loc[X.index[mask]])
+
+    X_train = pd.concat(X_list, axis=0).dropna()
+
+    scaler = RobustScaler()
+    Xs = scaler.fit_transform(X_train.to_numpy())
+
+    iso = IsolationForest(
+        n_estimators=500, contamination=contamination, random_state=random_state
+    )
+    iso.fit(Xs)
+
+    # seuil global sur les données nominales
+    score_train = -iso.score_samples(Xs)  # anomaly_score
+    thr = float(np.quantile(score_train, thr_quantile))
+
+    return {
+        "model": iso,
+        "scaler": scaler,
+        "threshold": thr,
+        "win": win,
+        "cols": {"time_col": time_col, "T_col": T_col, "P_col": P_col},
+    }
